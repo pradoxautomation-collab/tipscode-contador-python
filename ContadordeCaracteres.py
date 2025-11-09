8f58bdf071d508fed961196b53b71da7b87472dd
PradoxAutomation <pradoxautomation@gmail.com>
2025-11-08 23:51:55 -0300

    Programa contador usando len() - Projeto da @TiposCode

---
+++ ContadordeCaracteres.py
@@ -0,0 +1,5 @@
+# Primeiro Desafio, contagem de caracteres
+print("Bem vindo ao nosso contador de caracteres Pradox")
+print("_______________________")
+palavra = input("Digite a palavra: ")
+print(f"A palavra digitada foi {palavra} e a quantidade de caracteres é:", len(palavra))
